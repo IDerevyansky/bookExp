8d8826dd03403677b0c04c5283a3e6b798dbc2fe
“IDerevyansky” <IDerevyansky@yandex.ru>
2020-01-22 23:22:50 +0300

    0.0.4

---
+++ learn_Java/lesson12/learn_Java/Main.java
@@ -0,0 +1,10 @@
+package learn_Java;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Person person = new Person();
+		System.out.println(person.getName());
+	}
+	
+}
---
+++ learn_Java/lesson12/learn_Java/Person.java
@@ -0,0 +1,12 @@
+package learn_Java;
+
+public class Person {
+	private String mr = "mr.";
+	private String name = " Igor";
+	
+	
+	public String getName() {
+		return mr + name;
+	}
+	
+}
